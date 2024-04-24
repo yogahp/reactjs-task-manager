6d12ebed19a72b62adb06bf20b55fa613817f4a2
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-24 17:37:17 +0700

    Create Homepage

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 My Homepage</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My Homepage</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          {/* Add more navigation links here */}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
